0575dab92ceb29e7cfa9ca8de0bde47a6c3d21ca
Matthew <56852637+MatthewB07@users.noreply.github.com>
2026-09-04 23:46:37 -0400

    feat: transcription function

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from groq import AsyncGroq
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env.local")
 
 app = FastAPI()
 
+key = os.getenv("GROQ_API_KEY")
+client = AsyncGroq(api_key=key)
+
+async def transcribe(file: UploadFile):
+    try: 
+        audio_bytes = await file.read()
+
+        transcription = await client.audio.transcriptions.create(
+        file=(file.filename, audio_bytes),
+        model="whisper-large-v3-turbo",
+        temperature=0,
+        response_format="verbose_json",
+        )
+        return transcription.text
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code = 502, 
+            detail = "Transcription failed: " + str(e)
+        )
+
 @app.get("/")
 def read_root():
-    return {"message": "Song Finder"}
+    return {"Server": "Active"}
+
+@app.post("/find")
+async def find_song(file: UploadFile = File(...)):
+    try:
+        transcription = await transcribe(file)
+        return transcription
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code = 502, 
+            detail = "Find failed: " + str(e)
+        )
+
